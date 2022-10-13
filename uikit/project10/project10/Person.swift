a21e17f1d23f372d0f3b569dd519fa07511eff3d
Cassie Wallace <cassbwallace@gmail.com>
2022-10-12 18:00:14 -0600

    Project 10: Tutorial Code

---
+++ uikit/project10/project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Cassie Wallace on 10/12/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    // MARK: Var(s)
+    var name: String
+    var image: String
+    
+    // MARK: Init(s)
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
